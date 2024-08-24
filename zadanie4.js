88975178e06b425f3969f755942cbf0e844bc19e
Kaj Białas <kaj.bialas@dev-industry.com>
2024-08-24 13:31:19 +0200

    Zadanie 4

---
+++ zadanie4.js
@@ -0,0 +1,14 @@
+const faqTitles = document.querySelectorAll('.faq-title');
+
+faqTitles.forEach( title => {
+    title.addEventListener('click', function() {
+        console.log('click');
+        const content = this.nextElementSibling;
+
+        if (content.classList.contains('hidden')) {
+            content.classList.remove('hidden');
+        } else {
+            content.classList.add('hidden');
+        }
+    });
+});
