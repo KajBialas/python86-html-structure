6908b038ac302c889f15aeabd3e4382407e00c15
Kaj Białas <kaj.bialas@dev-industry.com>
2024-08-25 13:28:03 +0200

    Form js

--- script.js
+++ script.js
@@ -14,4 +14,32 @@ loadMoreButton.addEventListener('click', () => {
         }
 
     } 
-})
+});
+
+const postForm = document.getElementById('postForm');
+const postsContainer = document.querySelector('.posts-list');
+
+postForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const author = document.getElementById('author').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    const post = `
+                    <h2>
+                        ${title}
+                    </h2>
+                    <p>
+                        ${content}
+                    </p>
+    `;
+
+    const newPost = document.createElement('article');
+    newPost.classList.add('post');
+    newPost.innerHTML = post;
+
+    postsContainer.appendChild(newPost);
+
+    postForm.reset();
+});
